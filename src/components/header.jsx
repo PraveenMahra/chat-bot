b98e7db2ab81253fdc45a03e14f4afca55279680
PraveenMahra <praveen.mehra65@gmail.com>
2024-03-20 16:55:41 +0530

    add new chat button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,3 +1,23 @@
-export default function Header() {
-  return <div className="header">React Chatbot</div>;
+/* eslint-disable react/prop-types */
+
+function Header({ onSaveChat, onNewChat }) {
+  const handleButtonClick = () => {
+    if (onSaveChat) {
+      onSaveChat();
+    }
+    if (onNewChat) {
+      onNewChat();
+    }
+  };
+
+  return (
+    <div className="header">
+      <h1>ChatApp</h1>
+      <button className="header-btn" onClick={handleButtonClick}>
+        New Chat
+      </button>
+    </div>
+  );
 }
+
+export default Header;
